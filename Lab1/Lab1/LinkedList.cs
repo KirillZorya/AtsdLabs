9e03ccbd8e3e862a3b3c465f259ab9b647885b2d
KirillZorya <kyrylo.pashkevych@nure.ua>
2021-04-15 18:45:38 +0300

    Method First was added

--- Lab1/Lab1/LinkedList.cs
+++ Lab1/Lab1/LinkedList.cs
@@ -1,7 +1,19 @@
-﻿namespace Lab1
-{
-    class LinkedList<T>
-    {
+﻿using System;
 
+namespace Lab1
+{
+    class LinkedList<T> where T: IComparable<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        private int count;
+
+        public LinkedListNode<T> First
+        {
+            get 
+            { 
+                return head; 
+            }
+        }
     }
 }
